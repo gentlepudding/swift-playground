9c155f34bf42255ff05031f39fbfd62ca0def4b6
Maria Paradeeva <gentlepudding@gmail.com>
2016-12-08 22:56:53 +0500

    Update twin-peaks.swift

--- 2/twin-peaks.swift
+++ 2/twin-peaks.swift
@@ -1,1 +1,50 @@
-
+let totalGem = randomNumberOfGems 
+let expert = Expert()
+let character = Character()
+var gem = 0
+world.place(expert, facing: .north, atColumn: 1, row: 4)
+world.place(character, facing: .south, atColumn: 4, row: 6)
+func turnLock(up: Bool, numbersOfTimes: Int) {
+    for i in 1 ... numbersOfTimes {
+        if up == true {
+            expert.turnLockUp()
+        } else {
+            expert.turnLockDown()
+        }
+    }
+}
+func jump(distance: Int) {
+    for i in 1 ... distance {
+        if  character.isOnGem {
+            character.collectGem()
+            gem += 1
+        }
+        character.jump()
+    }
+}
+func move(distance: Int) {
+    for i in 1 ... distance {
+        if  character.isOnGem {
+            character.collectGem()
+            gem += 1
+        }
+        character.moveForward()
+    }
+}
+func generalFunc() {
+    move(distance: 6)
+    character.turnRight()
+    character.moveForward()
+    character.turnRight()
+    jump(distance: 6)
+    character.turnRight()
+    character.turnRight()
+    jump(distance: 6)
+    character.turnLeft()
+    character.moveForward()
+    character.turnLeft()
+}
+turnLock(up: true, numbersOfTimes: 3)
+while gem < totalGems {
+    generalFunc()
+}
